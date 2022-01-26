c4c1975989d814ac0fb0f9fe7cf2c4e7997961e7
Vanals <marco91vanali@gmail.com>
2022-01-26 08:59:54 +0000

    added resolver and type for movie, movies, user

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,34 @@
-const {UserList} = require('../MockData')
+const {UserList, MovieList} = require('../MockData')
 
 const resolvers = {
   Query: {
-    users() {
+    // USER RESOLVERS
+    users: () => {
       return UserList
+    },
+    // Resolver which takes and argument.
+    // parent = ?
+    user: (parent, args) => {
+      const id = args.id
+
+      const user = UserList.filter(user => {
+        return user.id === Number(id)
+      })
+
+      return user[0]
+    },
+
+    // MOVIE RESOLVERS
+    movies: () => {
+      return MovieList
+    },
+    movie: (parent, {name}) => {
+      // You can either do args.X(user example) or destructure like doing here
+      const movie = MovieList.filter(movie => {
+        return movie.name === name
+      })
+
+      return movie[0]
     }
   }
 }
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -5,6 +5,13 @@ const {gql} = require('apollo-server')
 
 //  "!" Exlamation mark makes a field required
 //  a type inside an Array [User], instead means we can return a list of that type of data.
+
+// enum are used to validate what you can return. Nationality was before of type String, now is type Nationality
+// an enum which that define exactly what values can be returned from the GraphQL API
+
+
+// In order to get favoriteMovies you don't want to add this info in the mock data, as it might not be in the database strucutr
+// Instead a resolver should take care of it..
 const typeDefs = gql`
 
   type User {
@@ -12,11 +19,31 @@ const typeDefs = gql`
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+  
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+  
+  enum Nationality {
+    GERMANY
+    CHILE
+    CANADA
+    BRAZIL
+    INDIA
   }
 `
 
